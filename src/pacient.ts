cc3495df6c633b8c50acd8ee02994c4b78d11dc9
Luiz <gustavoooliveira111@gmail.com>
2022-10-28 00:24:08 -0300

    adding pacient file

---
+++ src/pacient.ts
@@ -0,0 +1,11 @@
+export class Pacient {
+    name: string;
+    age: number
+    exam: string;
+
+    constructor(name: string,age: number, exam: string) {
+        this.name = name;
+        this.age = age;
+        this.exam = exam;
+    }
+}
