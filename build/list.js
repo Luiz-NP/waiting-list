e241241680a058111f850f098b95ca40a77b5967
Luiz <gustavoooliveira111@gmail.com>
2022-10-28 00:29:06 -0300

    deleting build folder

--- build/list.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.seeList = exports.list = void 0;
-exports.list = [];
-const seeList = () => {
-    return console.table(exports.list);
-};
-exports.seeList = seeList;
