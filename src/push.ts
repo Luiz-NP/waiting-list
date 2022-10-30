2f221cd53f664a049b4fbaf1208797f0d1a44d27
Luiz <gustavoooliveira111@gmail.com>
2022-10-29 21:16:04 -0300

    changing push file

--- src/push.ts
+++ src/push.ts
@@ -1,14 +1,9 @@
 import { Pacient } from "./pacient";
-import { options } from "./menu";
-import { list } from "./list";
+import { options } from "./exam";
+import { regular, preferential } from "./list";
 
 export const push = (pacient: (object | any), i: number): void => {
     let data: Pacient = new Pacient(pacient.name, pacient.age, options[i]);
 
-    if (pacient.age >= 60) {
-        let first: any = list[0];
-        let second: any = list[1];
-        
-          first.age > second.age ? list.splice(0, 0, data) : list.splice(second, 0, data);
-    } else list.push(data);
+    pacient.age >= 60 ? preferential.push(data) : regular.push(data);
 }
