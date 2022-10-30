170f8435f93bb825a8d501c74bed9262345abf6d
Luiz <gustavoooliveira111@gmail.com>
2022-10-29 21:14:21 -0300

    changing list file

--- src/list.ts
+++ src/list.ts
@@ -1,5 +1,16 @@
-export const list: object[] = [];
+type List = {
+    name: string;
+    age: number;
+    exam: string;
+}
+
+export const regular: List[] = [];
+export const preferential: List[] = [];
 
 export const seeList = (): void => {
-    return console.table(list)
+    console.log("regular list");
+    console.table(regular.length > 0 ? regular : "\n[no patients]\n");
+
+    console.log("preferential list");
+    console.table(preferential.length > 0 ? preferential : "\n[no patients]\n");
 }
